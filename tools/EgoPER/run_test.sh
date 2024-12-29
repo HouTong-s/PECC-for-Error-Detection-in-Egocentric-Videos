8bfd3a420090d1a1b0e31e744fbfc39384be17d9
Kraig Aue <49677236+HouTong-s@users.noreply.github.com>
2024-12-29 21:51:18 +0800

    Update run_test.sh

--- tools/EgoPER/run_test.sh
+++ tools/EgoPER/run_test.sh
@@ -1,28 +1,28 @@
 
 python test.py ./configs/EgoPER/coffee_aod_b1_n2_smooth_causal_gmm.yaml ./ckpt/EgoPER/coffee_aod_b1_n2_smooth_causal_gmm_final
-python metric_vis_egoper.py --task coffee --dirname coffee_aod_b1_n2_gaussian_causal_gmm_final/ --dirs EgoPER -as
+python metric_vis_egoper.py --task coffee --dirname coffee_aod_b1_n2_smooth_causal_gmm_final/ --dirs EgoPER -as
 python test_ed.py ./configs/EgoPER/coffee_aod_b1_n2_smooth_causal_gmm.yaml ./ckpt/EgoPER/coffee_aod_b1_n2_smooth_causal_gmm_final
-python metric_vis_egoper.py --task coffee --dirname coffee_aod_b1_n2_gaussian_causal_gmm_final/ --dirs EgoPER -ed
+python metric_vis_egoper.py --task coffee --dirname coffee_aod_b1_n2_smooth_causal_gmm_final/ --dirs EgoPER -ed
 
 python test.py ./configs/EgoPER/tea_aod_b1_n2_smooth_causal_gmm.yaml ./ckpt/EgoPER/tea_aod_b1_n2_smooth_causal_gmm_final
-python metric_vis_egoper.py --task tea --dirname tea_aod_b1_n2_gaussian_causal_gmm_final/ --dirs EgoPER -as
+python metric_vis_egoper.py --task tea --dirname tea_aod_b1_n2_smooth_causal_gmm_final/ --dirs EgoPER -as
 python test_ed.py ./configs/EgoPER/tea_aod_b1_n2_smooth_causal_gmm.yaml ./ckpt/EgoPER/tea_aod_b1_n2_smooth_causal_gmm_final
-python metric_vis_egoper.py --task tea --dirname tea_aod_b1_n2_gaussian_causal_gmm_final/ --dirs EgoPER -ed
+python metric_vis_egoper.py --task tea --dirname tea_aod_b1_n2_smooth_causal_gmm_final/ --dirs EgoPER -ed
 
 python test.py ./configs/EgoPER/quesadilla_aod_b1_n2_smooth_causal_gmm.yaml ./ckpt/EgoPER/quesadilla_aod_b1_n2_smooth_causal_gmm_final
-python metric_vis_egoper.py --task quesadilla --dirname quesadilla_aod_b1_n2_gaussian_causal_gmm_final/ --dirs EgoPER -as
+python metric_vis_egoper.py --task quesadilla --dirname quesadilla_aod_b1_n2_smooth_causal_gmm_final/ --dirs EgoPER -as
 python test_ed.py ./configs/EgoPER/quesadilla_aod_b1_n2_smooth_causal_gmm.yaml ./ckpt/EgoPER/quesadilla_aod_b1_n2_smooth_causal_gmm_final
-python metric_vis_egoper.py --task quesadilla --dirname quesadilla_aod_b1_n2_gaussian_causal_gmm_final/ --dirs EgoPER -ed
+python metric_vis_egoper.py --task quesadilla --dirname quesadilla_aod_b1_n2_smooth_causal_gmm_final/ --dirs EgoPER -ed
 
 python test.py ./configs/EgoPER/oatmeal_aod_b1_n2_smooth_causal_gmm.yaml ./ckpt/EgoPER/oatmeal_aod_b1_n2_smooth_causal_gmm_final
-python metric_vis_egoper.py --task oatmeal --dirname oatmeal_aod_b1_n2_gaussian_causal_gmm_final/ --dirs EgoPER -as
+python metric_vis_egoper.py --task oatmeal --dirname oatmeal_aod_b1_n2_smooth_causal_gmm_final/ --dirs EgoPER -as
 python test_ed.py ./configs/EgoPER/oatmeal_aod_b1_n2_smooth_causal_gmm.yaml ./ckpt/EgoPER/oatmeal_aod_b1_n2_smooth_causal_gmm_final
-python metric_vis_egoper.py --task oatmeal --dirname oatmeal_aod_b1_n2_gaussian_causal_gmm_final/ --dirs EgoPER -ed
+python metric_vis_egoper.py --task oatmeal --dirname oatmeal_aod_b1_n2_smooth_causal_gmm_final/ --dirs EgoPER -ed
 
 python test.py ./configs/EgoPER/pinwheels_aod_b1_n2_smooth_causal_gmm.yaml ./ckpt/EgoPER/pinwheels_aod_b1_n2_smooth_causal_gmm_final
-python metric_vis_egoper.py --task pinwheels --dirname pinwheels_aod_b1_n2_gaussian_causal_gmm_final/ --dirs EgoPER -as
+python metric_vis_egoper.py --task pinwheels --dirname pinwheels_aod_b1_n2_smooth_causal_gmm_final/ --dirs EgoPER -as
 python test_ed.py ./configs/EgoPER/pinwheels_aod_b1_n2_smooth_causal_gmm.yaml ./ckpt/EgoPER/pinwheels_aod_b1_n2_smooth_causal_gmm_final
-python metric_vis_egoper.py --task pinwheels --dirname pinwheels_aod_b1_n2_gaussian_causal_gmm_final/ --dirs EgoPER -ed
+python metric_vis_egoper.py --task pinwheels --dirname pinwheels_aod_b1_n2_smooth_causal_gmm_final/ --dirs EgoPER -ed
 
 
 
